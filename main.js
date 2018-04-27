0d6f68e8c23342f835457603fbbd8067dac5ca6d
Philip Zak <zakphi@gmail.com>
2018-04-26 22:47:55 -0400

    add helper code from SO

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ let computedStyle = window.getComputedStyle(square)
 let oldMarginLeft = computedStyle.getPropertyValue('margin-left')
 let boxWidth = computedStyle.getPropertyValue('width')
 let newMarginLeft = windowWidth - (parseInt(oldMarginLeft) + parseInt(boxWidth))
+
+/*
+  got lines 14-25 from https://stackoverflow.com/a/8742305
+*/
+let vendors = ['-moz-', '-webkit-', '-o-', '-ms-', ''];
+
+function toCamelCase(str) {
+  return str.toLowerCase().replace(/(\-[a-z])/g, function($1) {
+    return $1.toUpperCase().replace('-', '')
+  })
+}
+
+function setCss3Style(el, prop, val) {
+  for (let i = 0, l = vendors.length; i < l; i++)
+    el.style[toCamelCase(vendors[i] + prop)] = val
+}
